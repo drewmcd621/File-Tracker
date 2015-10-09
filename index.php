3e64164a244c4fd8a5b08697f3c34d7ebdc48ae8
drewmcd621 <mcdermotta@carnegiemuseums.org>
2015-10-09 17:10:26 -0400

    show errors

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
 require_once "./app/security.php";
 
 var_dump( login('mcdermotta@carnegiemuseums.org','') );
